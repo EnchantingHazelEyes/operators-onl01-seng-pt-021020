50eb86c698d9be17596273231f3bd64b8ce492c6
Samantha Barros <samantha.barros95@gmail.com>
2020-02-16 18:17:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-  if speed < 40 && speed > 60
+  if speed < 40 || speed > 60
    return "true"
   else 
    return "false"
  end
 end
 
-unsafe?(speed)
 
 def not_safe?(speed)
 	
